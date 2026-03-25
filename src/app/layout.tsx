46f0158bf00d96694b79b41a069e8b8f3b7f2708
Jihye817 <wlgul817@gmail.com>
2026-03-25 17:30:07 +0900

    chore: search console 연동 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>",
   },
+  verification: {
+    google: "rBSMTpPaCjU50Y22Y3iR7qrOHZodmFT6xVxBupxznBI",
+  },
 };
 
 export default function RootLayout({
